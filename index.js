092c1898dc67eefa2fc8793a494c493375a5f86c
Omar Nachar <omarnachar2@gmail.com>
2021-05-20 13:36:44 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+superbowlWin = (arrayofobjects) => {
+    let result = arrayofobjects.find( arrayofobjects => arrayofobjects.result === "W")
+    return !!result ? result.year : undefined
+}
