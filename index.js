264853dbb7b223760d63c4df4ac1fefc5b16d2a4
Omar Nachar <omarnachar2@gmail.com>
2021-05-20 13:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const testVar = {}
+function findMatching(drivers, name){
+  return drivers.filter( find_name => {
+    return (find_name.toLowerCase() === name.toLowerCase())
+   }
+  )
+}
 
-function testFunc() {
-  return "hi"
+function fuzzyMatch(drivers, letter){
+  return drivers.filter( find_name => {
+    if (letter.charAt(0) === find_name.charAt(0)){
+    return find_name
+    }
+  })
+}
+
+function matchName(drivers, name){
+  return drivers.filter( function(find_name){
+    if (find_name.name == name){
+      return find_name
+    }
+  })
 }
